97bae4505a146e3abf5db0aa95465b9c4f2bf7ba
YVrush <yury.verush@gmail.com>
2018-02-21 23:45:29 -0500

    Assignment-complete 21FEB2018

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,4 @@
-require_relative 'entry'
+require_relative 'entry.rb'
 
 class AddressBook
 
@@ -18,5 +18,14 @@ class AddressBook
        end
        entries.insert(index, Entry.new(name, phone_number, email))
    end
+
+   def remove_entry(name, phone_number, email)
+   entries.each do |entry|
+     if name == entry.name && phone_number == entry.phone_number && email == entry.email
+      to_delete = entry
+     end
+     entries.delete(to_delete)
+   end
+  end
 
 end
