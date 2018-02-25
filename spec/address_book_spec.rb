9348af4c83e66203342909e4796b1aa883311668
YVrush <yury.verush@gmail.com>
2018-02-25 02:34:42 -0500

    Assignment complete 24FEB2018

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -62,9 +62,7 @@ RSpec.describe AddressBook do
 
      it "imports the 1st entry" do
        book.import_from_csv("entries.csv")
-
        entry_one = book.entries[0]
-
        check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
 
      end
@@ -93,5 +91,31 @@ RSpec.describe AddressBook do
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
 
+   end
+
+   describe "#import_from_csv_2" do
+     it "imports the correct number of entries" do
+       book.import_from_csv("entries_2.csv")
+       book_size = book.entries.size
+       expect(book_size).to eq 3
+     end
+
+     it "imports the 1st entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_one = book.entries[0]
+       check_entry(entry_one, "Billy", "555-777-4854", "billy@glocmail.com")
+     end
+
+     it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bobby", "555-777-5415", "bobby@glocmail.com")
+     end
+
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Joyee", "555-777-3660", "joyee@glocmail.com")
+     end
    end
  end
